dd826ab3791dff4b987f195a4e16a8396862e714
Nicholas Gao <nicholas.gao96@gmail.com>
2018-10-19 20:25:01 +0100

    added color definitions to android

--- lib/item.dart
+++ lib/item.dart
@@ -1,10 +1,13 @@
 import 'package:intl/intl.dart';
 
 class Item extends Comparable<Item> {
-  DateTime addedDate;
   final String name;
   final String size;
   final String imageUrl;
+  String barcode;
+  String manufacturerNote;
+  Map<String, String> attributes;
+  DateTime addedDate;
   int amount;
 
   Item(this.name, {this.amount: 1, this.size : "", this.imageUrl : "https://www.maggi.de/Lists/Maggi-Images/maggi-kochstudio/wissen/lexikon/MAGGI-lexikon-cheddar.jpg", this.addedDate}){
